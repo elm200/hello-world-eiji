5e323126c8517c6a3262a2c358f938d978f283ca
Eiji Sakai <eijisakai@gmail.com>
2016-12-19 17:04:16 +0900

    Initial commit

---
+++ lib/hello/world/eiji.rb
@@ -0,0 +1,11 @@
+require "hello/world/eiji/version"
+
+module Hello
+  module World
+    module Eiji
+      def hello
+        puts "Hello world, Eiji!"
+      end
+    end
+  end
+end
---
+++ lib/hello/world/eiji/version.rb
@@ -0,0 +1,7 @@
+module Hello
+  module World
+    module Eiji
+      VERSION = "0.1.0"
+    end
+  end
+end
